ee2a6ffae465757c7709a86e0b3806ba63ad16ef
Pavel Mačák <pavel.macak@fs.cvut.cz>
2020-12-23 13:51:02 +0100

    Histogram update

--- histogram.py
+++ histogram.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # print(no_runs)
     count, bins, ignored = plt.hist(frame['Best value'], 15, density=False)
     binplt = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
-    plt.plot(binplt, count, linewidth=2, color='r') 
+    # plt.plot(binplt, count, linewidth=2, color='r') 
     plt.title("Histogram of results, Average runtime: " + str(int(avg_runtime)) + " s" )
     plt.xticks(bins,rotation=45)
     plt.ylabel("Number of runs in range")
